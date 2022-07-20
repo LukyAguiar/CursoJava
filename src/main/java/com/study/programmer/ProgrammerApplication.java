1356f4c8569822e4d3b012658493695847b12328
Lucas <lucas.aguiar@nitryx.com>
2022-07-20 17:18:36 -0300

    Teste

--- src/main/java/com/study/programmer/ProgrammerApplication.java
+++ src/main/java/com/study/programmer/ProgrammerApplication.java
@@ -3,6 +3,5 @@ package com.study.programmer;
 public class ProgrammerApplication {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
     }
 }
---
+++ src/main/java/com/study/programmer/domain/Studies.java
@@ -0,0 +1,14 @@
+package com.study.programmer.domain;
+
+public class Studies {
+    String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
